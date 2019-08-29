df75705aa3de6b3cb72c3966bc51806f39214456
Donny Davis <donny@fortnebula.com>
2019-08-29 10:38:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ WORKING_DIR=cl-openstack
 rm -rf $WORKING_DIR
 mkdir -p $WORKING_DIR
 curl -o $WORKING_DIR/cl-openstack.img.xz https://download.clearlinux.org/current/clear-$(curl https://download.clearlinux.org/latest)-cloudguest.img.xz
-unxz WORKING_DIR/cl-openstack.img.xz
+unxz $WORKING_DIR/cl-openstack.img.xz
