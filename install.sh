fb043a400d2cda3f259d9111fc42cd6b1248e210
Donny Davis <donny@fortnebula.com>
2019-08-29 10:50:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ curl -o $WORKING_DIR/cl-openstack.img.xz https://download.clearlinux.org/current
 unxz $WORKING_DIR/cl-openstack.img.xz
 mkdir -p $TMP_DIR
 echo "Mounting Image"
-sudo guestmount -a $WORKING_DIR/cl-openstack.img.xz -m /dev/sda2 $TMP_DIR
+sudo guestmount -a $WORKING_DIR/cl-openstack.img -m /dev/sda2 $TMP_DIR
 sudo cp openstack-config $TMP_DIR/usr/bin/
 sudo chmod +x $TMP_DIR/usr/bin/openstack-config
 sudo cp ucd-openstack.service $TMP_DIR/etc/systemd/system/ucd-openstack.service
-sudo guestumount $TMP_DIR
+sudo guestunmount $TMP_DIR
 echo "Scripts injected to image"
 
